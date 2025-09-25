6c16614de9dedba9166decb1da30196fe408fbd4
hassanboulidprog22-bit <hassan.boulid.prog22@gmail.com>
2025-09-26 00:07:30 +0100

    Update index.js

--- netlify/functions/index.js
+++ netlify/functions/index.js
@@ -1,1 +1,24 @@
+exports.handler = async function(event, context) {
+  const params = new URLSearchParams(event.queryStringParameters);
+  const username = params.get("username") || "Guest";
 
+  const html = `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8" />
+      <title>Welcome ${username}</title>
+    </head>
+    <body>
+      <h1>Hello, ${username}!</h1>
+      <p>This page was dynamically rendered on the backend.</p>
+    </body>
+    </html>
+  `;
+
+  return {
+    statusCode: 200,
+    headers: { "Content-Type": "text/html"},
+    body: html
+};
+};
